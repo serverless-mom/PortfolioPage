adadb247320f605d8f1d082d2a172fefafd5ecc5
tobyfee <tobyfee>
2015-05-14 15:09:11 -0700

    New Copy and images

--- js/functions.js
+++ js/functions.js
@@ -12,9 +12,9 @@ $(window).scroll(function(){
 	$('.foreground-character').css({
 		'transform':'translate(0px, '+ (wScroll/-30)  +'%)'
 	});
+//reveals the thumbnails once we've scrolled far enough down
 
-	
-  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.4)) {
 
     $('.clothes-pics figure').each(function(i){
 
@@ -24,6 +24,17 @@ $(window).scroll(function(){
     });
 
   }
-	//console.log(wScroll/2);
+  //controls the 'large image viewer'
+
+  if(wScroll > $('.large-window').offset().top - $(window).height()){
+
+    $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+
+    var opacity = (wScroll - $('.large-window').offset().top + 200) / (wScroll / 5);
+
+    $('.window-tint').css({'opacity': opacity});
+
+  }
+
 
 });
