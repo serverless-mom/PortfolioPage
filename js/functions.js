0a834c2da46279ef868eec9f64f97e61a29a3dbe
tobyfee <tobyfee>
2015-05-14 13:11:12 -0700

    image animations

--- js/functions.js
+++ js/functions.js
@@ -12,6 +12,18 @@ $(window).scroll(function(){
 	$('.foreground-character').css({
 		'transform':'translate(0px, '+ (wScroll/-30)  +'%)'
 	});
+
+	
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+
+    $('.clothes-pics figure').each(function(i){
+
+      setTimeout(function(){
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+
+  }
 	//console.log(wScroll/2);
 
 });
